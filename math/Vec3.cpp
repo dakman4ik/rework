f21fa10315ba9d6b7596a78216b0fd41996f41b6
dakman4ik <krd03052010@gmail.com>
2026-09-16 18:36:26 +0300

    добавлены перегрузки +,-,+=,-=. Добавлены функции dot и cross products, конструктор по умолчанию

--- math/Vec3.cpp
+++ math/Vec3.cpp
@@ -1,1 +1,33 @@
 #include "Vec3.hpp"
+
+Vec3::Vec3(float xx, float yy, float zz) : x(xx), y(yy), z(zz) {}
+Vec3::Vec3() : x(0.0), y(0.0), z(0.0) {}
+
+Vec3 Vec3::operator+(const Vec3& other) const{
+    return Vec3(other.x + this->x,other.y + this->y,other.z + this->z);
+}
+
+Vec3 Vec3::operator-(const Vec3& other) const{
+    return Vec3(this->x - other.x,this->y - other.y,this->z - other.z);
+}
+
+Vec3& Vec3::operator+=(const Vec3& other){
+    this->x += other.x;
+    this->y += other.y;
+    this->z += other.z;
+    return *this;
+}
+Vec3& Vec3::operator-=(const Vec3& other){
+    this->x -= other.x;
+    this->y -= other.y;
+    this->z -= other.z;
+    return *this;
+}
+
+Vec3 Vec3::cross(const Vec3& other) const{
+    return Vec3((this->y*other.z)-(this->z*other.y), (this->z*other.x)-(this->x*other.z), (this->x*other.y)-(this->y*other.x));
+}
+
+float Vec3::dot(const Vec3& other) const{
+    return this->x*other.x + this->y*other.y + this->z*other.z;
+}
--- math/Vec3.hpp
+++ math/Vec3.hpp
@@ -3,7 +3,16 @@
 class Vec3{
 public:
     float x,y,z;
-    Vec3(float x, float y, float z);
+
+    Vec3();
+    Vec3(float xx, float yy, float zz);
+
     Vec3 operator+ (const Vec3& other) const;
+    Vec3 operator- (const Vec3& other) const;
+
+    Vec3& operator+= (const Vec3& other);
+    Vec3& operator-= (const Vec3& other);
+
+    Vec3 cross(const Vec3& other) const;
     float dot(const Vec3& other) const;
 };
